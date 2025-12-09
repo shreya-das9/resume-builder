b13e309fcebd5fc577ca7a92851f1d58403127f7
Shreya Das <dshreya943@gmail.com>
2025-12-09 11:04:17 +0530

    Force redeploy

--- resume-builder/vite.config.js
+++ resume-builder/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// Force redeploy
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
